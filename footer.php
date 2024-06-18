e222d8de747587fb1d4322d928c7e202c11b531a
Leeyeyun <165612381+Leeyeyun@users.noreply.github.com>
2024-06-18 17:12:12 +0900

    한울 반응형 웹페이지

---
+++ footer.php
@@ -0,0 +1,34 @@
+<footer>
+    <div class="wrapper">
+        <div class="top">
+            <div class="section_wrap">
+                <!-- mobile에 나오는 통화버튼(초기값 숨기기) -->
+                <div class="m_call">
+                    <button type="button" class="call">053-214-5000</button>
+                    <button type="button" class="call"><img src="./images/call.svg" alt=""></button>
+                </div>
+                <section>
+                    <h3>한울 농업회사법인주식회사</h3>
+                    <p>
+                        대표자 : 박규섭<br>사업자번호 : 398-86-02326<br>전화번호 : 053-214-5000<br>주소 : 경상북도 영천시 청통면 관방길 153-20<br>E-mail : hanwool5000@hanmail.net
+                    </p>
+                </section>
+                <section>
+                    <h3>한울푸드</h3>
+                    <p>
+                        대표자 : 박규섭, 최윤지<br>사업자번호 : 502-31-44401<br>전화번호 : 053-214-5000<br>주소 : 경상북도 영천시 청통면 월성길 7-3<br>E-mail : hanwool5000@hanmail.net
+                    </p>
+                </section>
+            </div>
+            <a href="#"><img src="./images/logo_ft.png" alt="한울 기업"></a>
+        </div>
+        <div class="btm">
+            <p>&copy; Copyright HANWOOL.</p>
+            <div class="link">
+                <a href="#">Admin</a>
+                <a href="#">Smart Store</a>
+                <a href="#">Instagram</a>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ header.php
@@ -0,0 +1,72 @@
+<!--============================= header =============================-->
+<header>
+    <div class="top_wrap">
+        <h1><a href="./index.html"><img src="./images/logo.png" alt="볕이들다"></a></h1>
+        <nav>
+            <ul class="gnb">
+                <li><a href="./hanwool_info_1.html">한울소개</a>
+                    <ul class="sub">
+                        <li><a href="./hanwool_info_1.html">브랜드 스토리</a></li>
+                        <li><a href="#">브랜드 로고</a></li>
+                        <li><a href="#">상담 문의</a></li>
+                        <li><a href="#">기업연혁</a></li>
+                    </ul>
+                </li>
+                <li><a href="./product_info_2.html">제품소개</a>
+                    <ul class="sub">
+                        <li><a href="./product_info_2.html">제품소개</a></li>
+                        <li><a href="#">연구개발</a></li>
+                    </ul>
+                </li>
+                <li><a href="./certification_3.html">한울인증</a>
+                    <ul class="sub">
+                        <li><a href="./certification_3.html">특허인증</a></li>
+                    </ul>
+                </li>
+                <li><a href="./community_4.html">커뮤니티</a>
+                    <ul class="sub">
+                        <li><a href="./community_4.html">공지사항</a></li>
+                        <li><a href="#">한울뉴스</a></li>
+                        <li><a href="#">자연의 이야기</a></li>
+                    </ul>
+                </li>
+                <li><a href="./support_5.html">고객지원</a>
+                    <ul class="sub">
+                        <li><a href="./support_5.html">자주 묻는 질문</a></li>
+                        <li><a href="#">자료실</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+        <div class="lnb">
+            <a href="#" class="rectangle"></a>
+            <div class="lnb_bg"></div><!-- 사이드 열릴 때 왼쪽 배경 -->
+            <!-- desktop side nav -->
+            <div class="all_nav">
+                <ul>
+                    <li><a href="./hanwool_info_1.html">한울소개</a></li>
+                    <li><a href="./product_info_2.html">제품소개</a></li>
+                    <li><a href="./certification_3.html">한울인증</a></li>
+                    <li><a href="./community_4.html">커뮤니티</a></li>
+                    <li><a href="./support_5.html">고객지원</a></li>
+                </ul>
+                <div class="btm">
+                    <p class="info">
+                        경상북도 영천시 청통면 관방길 153-20<br>
+                        053-214-5000<br>
+                        hanwool5000@hanmail.net
+                    </p>
+                    <div class="link">
+                        <a href="#">Smart Store</a>
+                        <a href="#">Instagram</a>
+                    </div>
+                </div>
+            </div>
+            <!-- mobile side nav -->
+            <div class="m_nav">
+                <a href="#" class="all_nav_m"><img src="./images/m_nav_icon.svg" alt="모바일 메뉴 열기"></a>
+                <!-- 모바일 내비 제작 위치 -->
+            </div>
+        </div>
+    </div>
+</header>
